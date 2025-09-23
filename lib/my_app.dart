0ef364625b13793eab7b3fcd44d5dc18ef081b65
Si Dang Van <si.dangvan@everisconnect.onmicrosoft.com>
2025-09-22 20:09:57 -0700

    Initial commit

---
+++ lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('data');
+  }
+}
